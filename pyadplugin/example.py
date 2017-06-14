ead68bb0753596d0c4b4e4f4ee616c3ce15e0bb3
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-13 18:14:56 -0700

    add comments

--- pyadplugin/example.py
+++ pyadplugin/example.py
@@ -9,13 +9,16 @@ logger = logging.getLogger(__name__)
 
 
 def wfsum(array):
-    # time.sleep(1)
+    # time.sleep(1)  # To check how it works with slow plugins
     # logger.debug('we did a callback')
     return sum(array)
 
 
+# Switch this to a camera you'd like to test with
+ad_prefix = 'HX2:SB1:CVV:01:'
+
 server = ADPluginServer(prefix='SUM:',
-                        ad_prefix='HX2:SB1:CVV:01:',
+                        ad_prefix=ad_prefix,
                         stream='IMAGE1',
                         min_cbtime=10,
                         enable_callbacks=True)
