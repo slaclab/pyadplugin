303fff862781c6632f6b2365c1f1647be62ccc28
Zachary Lentz <zlentz@slac.stanford.edu>
2017-06-13 18:35:54 -0700

    Clear the has_update flag when we've gotten an array

--- pyadplugin/server.py
+++ pyadplugin/server.py
@@ -17,20 +17,64 @@ class ADPluginServer:
     Server to mimic some of the plugin records/functionality from AreaDetector.
     The goal was for the user to only need to supply configuration parameters
     and functions to run that take the image np.ndarray as an argument. These
-    servers have the following PVs: (set them from the equivalent non-_RBV PV)
+    servers have the following PVs: (set them from the non-_RBV PV)
 
     Attributes
     ----------
-    $(ad_prefix)$(prefix)NDArrayPort_RBV:
+    $(ad_prefix)$(prefix)NDArrayPort{_RBV}:
         In AD this is a port name, but here we have to take from an array pv.
         This is where we store the stream argument e.g. IMAGE1
 
-    $(ad_prefix)$(prefix)EnableCallbacks:
+    $(ad_prefix)$(prefix)EnableCallbacks{_RBV}:
         If this is set to 0, The plugin will be disabled. Otherwise, the plugin
         will be active.
+
+    $(ad_prefix)$(prefix)MinCallbackTime{_RBV}:
+        If running the callback took less than this many seconds, we'll sleep
+        until that time has expired before running another callback.
+
+    $(ad_prefix)$(prefix)QueueSize{_RBV}:
+        Maximum number of arrays kept in the memory queue. This can be useful
+        if the source updates at an unstable rate.
+
+    $(ad_prefix)$(prefix)QueueUse{_RBV}:
+        Number of arrays currently in the queue.
+
+    $(ad_prefix)$(prefix)DroppedArrays{_RBV}:
+        Number of arrays we didn't put into the queue because the queue was
+        full.
+
+    $(ad_prefix)$(prefix){USER_DEFINED}:
+        Whatever is installed using the ADPluginPV class.
     """
     def __init__(self, prefix, ad_prefix, stream,
                  enable_callbacks=0, min_cbtime=0, queuesize=5):
+        """
+        Parameters
+        ----------
+        prefix: str
+            The plugin prefix that comes after the areaDetector prefix in the
+            PV names.
+
+        ad_prefix: str
+            The base areaDetector control prefix. This should match a real
+            areaDetector IOC's prefix.
+
+        stream: str
+            The image stream to use for the plugins. We'll be using:
+                $(ad_prefix)$(stream)ArrayData    for values
+                $(ad_prefix)$(stream)UniqueId_RBV for update monitoring
+
+        enable_callbcaks: bool, optional
+            If True, start the IOC with callbacks enabled. Start disabled
+            otherwise.
+
+        min_cbtime: float, optional
+            The initial value for the minimum time for each callback loop.
+
+        queuesize: int, optional
+            The initial value for the array queue. The default is 5.
+        """
         self.server = PypvServer(ad_prefix + prefix)
         self.ad_prefix = ad_prefix
         self.ad_directory = {}
@@ -61,6 +105,9 @@ class ADPluginServer:
         plugins.start()
 
     def _add_builtin(self, name, value, cb=None):
+        """
+        Does setup for the built-in PVs, which are all basically the same.
+        """
         rbv = PyPV(name + '_RBV', value=value, server=self.server)
         if cb is None:
             def cb(**kwargs):
@@ -75,18 +122,31 @@ class ADPluginServer:
         self.ad_directory[setter.name] = setter
 
     def install_plugin(self, plugin):
+        """
+        Called by ADPluginPV to register itself with the server.
+        """
         with self.settings_lock:
             self.plugins[plugin.name] = plugin
 
     def uninstall_plugin(self, plugin):
+        """
+        You can call this to remove a plugin.
+        """
         with self.settings_lock:
             del self.plugins[plugin.name]
 
     def _ndarray_port_cb(self, *, value, **kwargs):
+        """
+        Setter for puts to the enable ndarray port PV.
+        When we change the stream, we need new source PVs.
+        """
         with self.settings_lock:
             self._initialize_pv(value)
 
     def _enable_callbacks_cb(self, *, value, **kwargs):
+        """
+        Setter for puts to the enable callbacks PV.
+        """
         with self.settings_lock:
             if value:
                 self.enable_callbacks = True
@@ -94,12 +154,18 @@ class ADPluginServer:
                 self.enable_callbacks = False
 
     def _min_cbtime_cb(self, *, value, **kwargs):
+        """
+        Setter for puts to the minimum callback time pv
+        """
         with self.settings_lock:
             if value < 0:
                 value = 0
             self.min_cbtime = value
 
     def _queuesize_cb(self, *, value, **kwargs):
+        """
+        Setter for puts to the queuesize pv.
+        """
         with self.settings_lock:
             if value < 1:
                 value = 1
@@ -112,6 +178,9 @@ class ADPluginServer:
                 self.queue = new_queue
 
     def _initialize_pv(self, stream):
+        """
+        Set up the data and update monitoring pvs for the chosen stream.
+        """
         get_pvname = self.ad_prefix + stream + ':ArrayData'
         logger.debug('getting data from %s', get_pvname)
         self.get_pv = PV(get_pvname)
@@ -123,6 +192,9 @@ class ADPluginServer:
                          auto_monitor=True)
 
     def _mark_has_update(self, **kwargs):
+        """
+        Callback to indicate that a new value is ready.
+        """
         self.has_update.set()
 
     def _update_queue_use(self):
@@ -158,6 +230,7 @@ class ADPluginServer:
                         logger.debug('dropped %s arrays total', n_dropped)
                     else:
                         array = self.get_pv.get()
+                        self.has_update.clear()
                         logger.debug('we got an array, stashing into queue')
                         queue.put(array)
                         self._update_queue_use()
